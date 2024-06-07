6a17988153c0e357d79750390731d875b437ba3e
monitz <igorb.kuhl@gmail.com>
2024-06-07 19:13:58 -0300

    Added keymaps to switch between Vim windows.

--- lua/moni/remap.lua
+++ lua/moni/remap.lua
@@ -21,3 +21,7 @@ vim.keymap.set("n", "<leader>f", function()
 end)
 
 vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
