9873dd63f123f0fc65e0de69c6fb70ee45e11d29
liz <igorb.kuhl@gmail.com>
2024-08-13 12:51:29 -0300

    Remane user from moni to liz

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-require("moni")
+require("liz")
 print("Welcome back~")
--- lua/moni/init.lua
+++
@@ -1,5 +0,0 @@
-require("moni.remap")
-require("moni.plug")
-require("moni.set")
-require("mason").setup()
-require("code_runner").setup()
